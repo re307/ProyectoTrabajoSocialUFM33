1b3489713950111cf3e4bcf3b4b4bcf6053ee43f
Rafael Eduardo <raed307@gmail.com>
2021-09-25 17:50:46 -0500

    TSadmin - Inicio de ramal
    En este ramal se trabajara con la base datos antes de iniciar el
    proyecto en MVC, una ves terminado las adecuaciones iniciales a la base
    de datos diseña se pasara el ramal de desarrollo para ir realizando los
    cambios junsto con la explotacion de los datos por parte de la
    aplicacion web.

---
+++ ControlSguimientoPacientesTrabajoSocialUMF33.sql
@@ -0,0 +1,38 @@
+﻿--Control general de pacientes para Trabajo Social unidad medicina familiar 33
+create table ControlPacientes_InformacionGeneral(
+	Id int identity(1,1)
+	,ControlPacientes_InformacionGeneral_Pacientes_Id int
+	--,ControlPacientes_InformacionGeneral_Consultorio_Id int
+	,ControlPacientes_InformacionGeneral_Estatus_Id int
+	,ControlPacientes_Seguimiento_TrabajadoraSocial_Id int
+	,RegistroGenerado_TimeStamp datetime
+	,RegistroGenerado_UsuarioId nvarchar(128)
+)
+--Catalogo de Pacientes
+create table ControlPacientes_InformacionGeneral_Pacientes(
+	Id int identity(1,1)
+	,ApellidoPaterno varchar(max)
+	,ApellidoMaterno varchar(max)
+	,Nombres varchar(max)
+	,NumeroSeguridadSocial varchar(max)
+	,Consultorio varchar(max)
+	,EsActivo bit
+	,RegistroGenerado_TimeStamp datetime
+	,RegistroGenerado_UsuarioId nvarchar(128)
+)
+--Catalogo de estatus
+create table ControlPacientes_InformacionGeneral_Estatus(
+	Id int identity(1,1)
+	,Estatus varchar(max)
+	,EsActivo bit
+	,RegistroGenerado_TimeStamp datetime
+	,RegistroGenerado_UsuarioId nvarchar(128)
+)
+--Seguimiento Pacientes es el registro donde las Trabajadoras Sociles llevaran
+--su registro y posterior compararla información en Informacion General
+create table ControlPacientes_Sguimiento(
+	Id int identity(1,1)
+	,ControlPacientes_InformacionGeneral_Id int
+	,RegistroGenerado_TimeStamp datetime
+	,RegistroGenerado_UsuarioId nvarchar(128)
+)
